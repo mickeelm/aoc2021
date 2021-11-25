4036663d0d1da20a2a0b846798cfe8d3871296cd
Micke Elm <elm.micke@gmail.com>
2021-11-25 09:03:56 +0100

    Refactored solution a bit, added real answer test

--- day01/src/main/kotlin/PreAoc.kt
+++ day01/src/main/kotlin/PreAoc.kt
@@ -1,5 +1,4 @@
 import java.io.File
-import java.util.*
 
 fun main() {
     val input = File("input.txt").readLines().map { it.toInt() }
@@ -22,29 +21,31 @@ private class IndexedNonPrime(number: Int, index: Int) {
     val value = if (index % 2 == 0) number else -number
 }
 
-private fun List<Int>.indexedNonPrimes(): Set<IndexedNonPrime> =
-    primes(this).let {
-        mapIndexedNotNull { index, number -> if (!it.contains(number)) IndexedNonPrime(number, index) else null }
-            .toSet()
-    }
-
 private fun List<Int>.indexedPrimes(): Set<IndexedPrime> =
     primes(this).let {
         mapIndexedNotNull { index, number -> if (it.contains(number)) IndexedPrime(number, index) else null }
             .toSet()
     }
 
+private fun List<Int>.indexedNonPrimes(): Set<IndexedNonPrime> =
+    subtract(primes(this)).let {
+        mapIndexedNotNull { index, number -> if (it.contains(number)) IndexedNonPrime(number, index) else null }
+            .toSet()
+    }
+
 private fun primes(integers: List<Int>): Set<Int> {
     val maxValue = integers.maxOf { it }
-    val primeMap = (2..maxValue).associateWith { true }.toSortedMap()
+    val primeFlags = MutableList(maxValue - 1) { true }
 
-    return detectPrimesRecursive(primeMap, -1)
+    return detectPrimesRecursive(primeFlags)
 }
 
-private fun detectPrimesRecursive(primeMap: SortedMap<Int, Boolean>, lastPrime: Int): Set<Int> =
-    primeMap.firstNotNullOfOrNull { if (it.value && it.key > lastPrime) it.key else null }?.let { nextPrime ->
-        val maxValue = primeMap.lastKey()
-        (nextPrime..maxValue step nextPrime).drop(1).forEach { primeMap[it] = false }
-        detectPrimesRecursive(primeMap, nextPrime)
-
-    } ?: primeMap.filter { it.value }.keys
+private fun detectPrimesRecursive(primeFlags: MutableList<Boolean>, lastPrime: Int? = null): Set<Int> {
+    val dropCount = lastPrime?.minus(1) ?: 0
+    val nextPrime = primeFlags.drop(dropCount).indexOfFirst { it }.let { if (it >= 0) it + dropCount + 2 else null }
+    return nextPrime?.run {
+        val maxNumber = primeFlags.size + 1
+        (nextPrime..maxNumber step nextPrime).drop(1).forEach { primeFlags[it - 2] = false }
+        return detectPrimesRecursive(primeFlags, nextPrime)
+    } ?: primeFlags.mapIndexedNotNull { index, flag -> if (flag) index + 2 else null }.toSet()
+}
