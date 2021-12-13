995e5c1abb4b7e8c84f6d7d487de320fb07a0e33
Micke Elm <elm.micke@gmail.com>
2021-12-13 09:31:17 +0100

    this is nicer

--- day12/src/main/kotlin/AocDay12.kt
+++ day12/src/main/kotlin/AocDay12.kt
@@ -9,10 +9,9 @@ fun main() {
     println(answer)
 }
 
-fun List<String>.caveConnections() =
-    flatMap { connection -> connection.split('-').zip(connection.split('-').reversed()) }
-        .groupBy { it.first }
-        .mapValues { entry -> entry.value.map { it.second }.filter { it != "start" }.toSet() }
+fun List<String>.caveConnections() = flatMap { it.split('-').zip(it.split('-').reversed()) }
+    .groupBy { it.first }
+    .mapValues { entry -> entry.value.map { it.second }.filter { it != "start" }.toSet() }
 
 fun solutionPart1(input: List<String>) = input.caveConnections().countPaths("start")
 fun solutionPart2(input: List<String>) = input.caveConnections().countPaths("start", smallTwice = true)
